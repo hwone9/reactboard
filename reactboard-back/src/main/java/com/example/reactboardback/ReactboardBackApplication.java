2873035be04728164ce7e059038246fb8fa6969f
hwone9 <hwone9@gmail.com>
2024-01-18 10:04:11 +0900

    back init

---
+++ reactboard-back/src/main/java/com/example/reactboardback/ReactboardBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactboardback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactboardBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactboardBackApplication.class, args);
+	}
+
+}
---
+++ reactboard-back/src/test/java/com/example/reactboardback/ReactboardBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactboardback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactboardBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
